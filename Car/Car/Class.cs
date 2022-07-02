0391ead3d855223cb7973e17bf4e0caa2437d4a1
hama khalid <m_jamal@utb.cz>
2022-07-02 22:12:07 +0200

    Initial Commit

---
+++ Car/Car/Class.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car
+{
+    class Class
+    {
+        public string Model;
+        public int Year;
+        public double StartingKm;
+        public double EndingKm;
+        public double FuelConsumption;
+        public double TravelTime;
+
+        public double GetTripLength()
+        {
+            double TripLength = EndingKm - StartingKm;
+            return TripLength;
+        }
+        public double GetSpeed()
+        {
+            double Speed = GetTripLength() / TravelTime;
+            return Speed;
+        }
+        public double GetFuelEfficiency()
+        {
+            double FuelEfficiency = GetTripLength() / (FuelConsumption / 100);
+            return FuelEfficiency;
+        }
+        public void ClassifyCar()
+        {
+            double age = DateTime.Now.Year - Year;
+            if (age <= 1)
+            {
+                Console.WriteLine("New Car, Enjoy it");
+            }
+            else if (age <= 3)
+            {
+                Console.WriteLine("Semi-Used Car, How is it going");
+            }
+            else if (age <= 5)
+            {
+                Console.WriteLine("Used-Car, Time for looking a new Car");
+            }
+            else if (age > 5)
+            {
+                Console.WriteLine("Old Car, Please Change it");
+            }
+        }
+    }
+}
---
+++ Car/Car/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Car
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Class User = new Class();
+
+            Console.WriteLine("Enter Your Car name: ");
+            string CarName = Console.ReadLine();
+            Console.WriteLine("Enter Car year: ");
+            int CarYear = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the kilometers at the beginning: ");
+            double StaringK = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the kilometers at the end: ");
+            double EndingK = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the amount of fuel spent in liters: ");
+            double FuelC = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the trip length in hours: ");
+            double TravelT = double.Parse(Console.ReadLine());
+
+            User.Model = CarName;
+            User.Year = CarYear;
+            User.StartingKm = StaringK;
+            User.EndingKm = EndingK;
+            User.FuelConsumption = FuelC;
+            User.TravelTime = TravelT;
+
+            Console.WriteLine($"This is the trip length in km {User.GetTripLength()}");
+            Console.WriteLine($"This is the average speed of the car {User.GetSpeed()}");
+            Console.WriteLine($"This is the amount of liters per km {User.GetFuelEfficiency()}");
+            User.ClassifyCar();
+        }
+    }
+}
